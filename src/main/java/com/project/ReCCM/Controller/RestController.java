3bc687b9b2a0b1706e69a9fa051bccb73e028810
Choi <epqmfwjs@naver.com>
2024-09-03 20:26:22 +0900

    커피리스트 조회 자바스크립트 및 컨틀롤러 시작

--- src/main/java/com/project/ReCCM/Controller/RestController.java
+++ src/main/java/com/project/ReCCM/Controller/RestController.java
@@ -1,4 +1,26 @@
 package com.project.ReCCM.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/api")
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @GetMapping("/coffeeList")
+    public List<String> coffeeList() {
+        System.out.println("커피리스트 조회 들어옴");
+
+        // 리스트 생성
+        List<String> coffeeList = new ArrayList<>();
+        coffeeList.add("Espresso - 3000원");
+        coffeeList.add("Latte - 4000원");
+        coffeeList.add("Cappuccino - 4500원");
+
+        // 리스트 반환
+        return coffeeList;
+    }
 }
