ecf586b56045e562951cef9950d88205b7e55fbe
Choi <epqmfwjs@naver.com>
2024-09-09 16:27:19 +0900

    DB 연결 및 커피리스트 조회 기능 구현중

--- src/main/java/com/project/ReCCM/controller/RestController.java
+++ src/main/java/com/project/ReCCM/controller/RestController.java
@@ -1,5 +1,9 @@
 package com.project.ReCCM.controller;
 
+import com.project.ReCCM.domain.CoffeeList;
+import com.project.ReCCM.domain.CoffeeListRepository;
+import com.project.ReCCM.service.CoffeeListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,15 +14,22 @@ import java.util.List;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
+    @Autowired
+    private CoffeeListService coffeeListService;
+    @Autowired
+    private CoffeeListRepository coffeeListRepository;
+
     @GetMapping("/coffeeList")
-    public List<String> coffeeList() {
+    public List<CoffeeList> coffeeList() {
         System.out.println("커피리스트 조회 들어옴");
 
-        // 리스트 생성
-        List<String> coffeeList = new ArrayList<>();
-        coffeeList.add("Espresso - 3000원");
-        coffeeList.add("Latte - 4000원");
-        coffeeList.add("Cappuccino - 4500원");
+        List<CoffeeList> coffeeList = coffeeListRepository.findAll();
+
+        // 리스트 생성 db 연결전 임시 데이터
+//        List<String> coffeeList = new ArrayList<>();
+//        coffeeList.add("Espresso - 3000원");
+//        coffeeList.add("Latte - 4000원");
+//        coffeeList.add("Cappuccino - 4500원");
 
         // 리스트 반환
         return coffeeList;
